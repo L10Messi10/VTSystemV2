9a7848eebadccb7b12c3d56a607c393b1e4bf812
L10Messi10 <loveyourenemy@outlook.com>
2021-07-20 12:52:26 +0800

    some modification

--- VTSystemV2/View/RegisterStudentWindow.xaml.cs
+++ VTSystemV2/View/RegisterStudentWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace VTSystemV2.View
     public partial class RegisterStudentWindow : Window
     {
         private string _selectedFileName;
+        public static bool Xadd;
         Byte[] _imagebytearray;
         private BitmapImage bitmap;
         public RegisterStudentWindow()
@@ -36,14 +37,11 @@ namespace VTSystemV2.View
         Notifier notifier = new Notifier(cfg =>
         {
             cfg.PositionProvider = new WindowPositionProvider(
-                parentWindow: Application.Current.MainWindow,
-                corner: Corner.BottomRight,
-                offsetX: 10,
-                offsetY: 10);
+                Application.Current.MainWindow, Corner.BottomRight, 10, 10);
 
             cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
-                notificationLifetime: TimeSpan.FromSeconds(3),
-                maximumNotificationCount: MaximumNotificationCount.FromCount(5));
+                TimeSpan.FromSeconds(3),
+                MaximumNotificationCount.FromCount(5));
 
             cfg.Dispatcher = Application.Current.Dispatcher;
         });
@@ -51,6 +49,7 @@ namespace VTSystemV2.View
         private void BtnBrowse_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.png) | *.jpg; *.jpeg; *.jpe; *.png";
             if (dlg.ShowDialog() == true)
             {
                 _selectedFileName = dlg.FileName;
@@ -130,36 +129,106 @@ namespace VTSystemV2.View
         {
             if (studid.Text == "")
             {
-                notifier.ShowError("Please enter Student's Id #");
+                notifier.ShowError("Please enter Student's Id #.");
                 studid.Focus();
                 return;
             }
             if (fname.Text == "")
             {
-                notifier.ShowError("Please enter Student's First name");
+                notifier.ShowError("Please enter Student's First name.");
                 fname.Focus();
                 return;
             }
             if (lname.Text == "")
             {
-                notifier.ShowError("Please enter Student's Last name");
-                lname.Focus();
+                notifier.ShowError("Please enter Student's Last name.");
+                course.Focus();
                 return;
             }
             if (minitial.Text == "")
             {
-                notifier.ShowError("Please enter Student's Middle initial");
+                notifier.ShowError("Please enter Student's Middle initial.");
+                minitial.Focus();
+                return;
+            }
+            if (course.Text == "")
+            {
+                notifier.ShowError("Please Select Student's Course.");
                 fname.Focus();
+                return;
+            }
+            if (yrlevel.Text == "")
+            {
+                notifier.ShowError("Please Select Student's Year level.");
+                yrlevel.Focus();
+                return;
+            }
+            if (vtCategory.Text == "")
+            {
+                notifier.ShowError("Please Select Voter's category");
+                vtCategory.Focus();
                 return;
             }
             saveprogress.Visibility = Visibility.Visible;
             await Xaddmode();
             saveprogress.Visibility = Visibility.Collapsed;
         }
+        private async Task XDisplayEdit()
+        {
+            byte[] img;
+            //bunifuTextBox1.Text = Form1.XID;
+            Sqlcmd.Parameters.Clear();
+            await Conopen();
+            Strsql = "Select * from tbl_Students where stud_id = '" + studid.Text + "'";
+            Sqlcmd.CommandText = Strsql;
+            Sqlcmd.Connection = Cnn;
+            Sqladapter.SelectCommand = Sqlcmd;
+            Sqlreader = Sqlcmd.ExecuteReader();
+            //metroComboBox1.Items.Clear();
+            if (Sqlreader.Read())
+            {
+                studid.Text = Sqlreader.GetValue(0).ToString();
+                fname.Text = Sqlreader.GetValue(1).ToString();
+                lname.Text = Sqlreader.GetValue(2).ToString();
+                minitial.Text = Sqlreader.GetValue(3).ToString();
+                course.Text = Sqlreader.GetValue(5).ToString();
+                yrlevel.Text = Sqlreader.GetValue(6).ToString();
+                img = (byte[])Sqlreader.GetValue(7);
+                if (img.Length != 0)
+                {
+                    //studimg.ImageSource = (ImageBrush)Image.FromStream(new MemoryStream(img));
+                    studimg.ImageSource = LoadImage(img);
+                }
+
+            }
+            Sqlcmd.Dispose();
+            Sqlreader.Close();
+            Cnn.Close();
+            Strsql = "";
+        }
+        //Convert byte image to imagesource
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
         private async Task Xaddmode()
         {
-            //try
-            //{
+            try
+            {
+                //This still needs to edit if the students had no picture, the system must still sav the file.
                 Sqlcmd.Parameters.Clear();
                 Bitmap temp = new(_selectedFileName);
                 MemoryStream strm = new();
@@ -177,37 +246,29 @@ namespace VTSystemV2.View
                 Sqlcmd.Parameters.AddWithValue("@Yr_Level", yrlevel.Text);
                 Sqlcmd.Parameters.AddWithValue("@stud_img", _imagebytearray);
                 Sqlcmd.Parameters.AddWithValue("@vtc_desc", vtCategory.Text);
-            Sqlcmd.Connection = Cnn;
-                //sqlcmd.CommandType = CommandType.Text;
+                Sqlcmd.Connection = Cnn;
                 Sqlcmd.CommandText = Strsql;
                 await Sqlcmd.ExecuteNonQueryAsync();
-                //var a = new T_Message();
-                //var frmok = new FrmOk();
-                //a.Show(this);
-                //frmok.titletxt.Text = "Success";
-                //frmok.msgtxt.Text = "Record has been successfully Added! Please refresh the student data to see modified changes.";
-                //frmok.OkDescription = "Success";
-                //frmok.ShowDialog(this);
-                //a.Hide();
                 Sqlcmd.Dispose();
                 Strsql = "";
                 Cnn.Close();
                 Close();
+                notifier.ShowSuccess("Data successfully saved!");
 
-            //}
-            //catch
-            //{
-            //    //var a = new T_Message();
-            //    //var frmok = new FrmOk();
-            //    //a.Show(this);
-            //    //frmok.titletxt.Text = "Error";
-            //    //frmok.msgtxt.Text = "The ID # you've Entered is already in the record! Please verify this problem to the registrar!";
-            //    //frmok.OkDescription = "Error";
-            //    //frmok.ShowDialog(this);
-            //    //a.Hide();
-            //    //studid.Text = "";
-            //    //studid.Focus();
-            //}
+            }
+            catch
+            {
+                //var a = new T_Message();
+                //var frmok = new FrmOk();
+                //a.Show(this);
+                //frmok.titletxt.Text = "Error";
+                //frmok.msgtxt.Text = "The ID # you've Entered is already in the record! Please verify this problem to the registrar!";
+                //frmok.OkDescription = "Error";
+                //frmok.ShowDialog(this);
+                //a.Hide();
+                //studid.Text = "";
+                //studid.Focus();
+            }
 
         }
 
